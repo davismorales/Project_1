55442f9250df5eaa149ecc9179500b9877101002
Davis <davis.morales@courts.mo.gov>
2024-02-20 15:53:49 -0600

    Partially Finished React Tutorial

--- src/main/java/com/api/nbaApi/service/NbaApiWebServiceImpl.java
+++ src/main/java/com/api/nbaApi/service/NbaApiWebServiceImpl.java
@@ -1,5 +1,6 @@
 package com.api.nbaApi.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import kong.unirest.HttpResponse;
@@ -9,9 +10,12 @@ import kong.unirest.Unirest;
 @Service
 public class NbaApiWebServiceImpl {
 
+	@Value("${authentication.rapidAPIKey}")
+	private String rapidAPIKey;
+
 	public String getPlayers() {
 		HttpResponse<String> response = Unirest.get("https://api-nba-v1.p.rapidapi.com/players?season=2023")
-			.header("X-RapidAPI-Key", "69d2a15b99msha85cb38973dc1bbp13505ajsnb87ce882d69b")
+			.header("X-RapidAPI-Key", rapidAPIKey)
 			.header("X-RapidAPI-Host", "api-nba-v1.p.rapidapi.com")
 			.asString();
 
